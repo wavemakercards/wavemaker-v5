d9542611d9bf9837235bd6d64274067886a7916f
Iain Wood <cards@wavemaker.co.uk>
2025-03-10 19:41:21 +0000

    refactor Home component layout and styles; update font family and enhance toolcard design

--- src/components/Apps/Home/Home.vue
+++ src/components/Apps/Home/Home.vue
@@ -14,28 +14,90 @@ export default {
 
 <template>
   <div class="project-home" v-if="this.$root.syncdb.Settings">
-    <div style="display: flex; justify-content: center;">
+    <div style="text-align: center;">
       <Logo style="width: 150px;height: 150px;"/>
     </div>
-    <h1>Get Started writing</h1>
-    <label>You can change the Project name here</label>
-    <h2><ProjectName /></h2>
-    {{ this.$root.databaseName }}
-    <button
+  
+    <h2 class="inputstyle"><ProjectName /></h2>
+    <!--
+    
+     <button
     v-if="$root.mainwindow"
     @click="$root.openNewWindow()">New Window</button>
+    -->
+   
+    <div class="toolcard-grid">
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+      <div class="toolcard">
+      <h2>Write</h2>
+      <p>Write your novel here</p>
+      </div>
+    </div>
+  
+  
+  
+  
   </div>
 </template>
 
 <style scoped>
+
+.inputstyle{
+  background-color: var(--bg3);
+  padding: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  font-size: 1.3rem;
+  text-align: center;
+  border-radius: 10px;
+}
 .project-home {
-  max-width: 700px;
+  max-width: 1000px;
   margin: 0 auto;
-  margin-top: 20px;
   padding: 50px;
-  background-color: var(--neutral-light);
-  color: var(--neutral-dark);
-  height: calc(100% - 40px);
-  border:2px solid #212121;
+}
+
+.toolcard-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.toolcard{
+  background-color: #fff;
+  border-radius: 10px;
+  color: #212121;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease-in-out;
+  cursor: pointer;
+}
+
+.toolcard:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
 }
 </style>
